57031151cd5ee5772b2252f3942b4965e828e2c5
Kyu <kyudeci@gmail.com>
2018-11-03 20:11:13 -0400

    Revert button active

--- test.js
+++ test.js
@@ -5,9 +5,13 @@ let imgR = document.getElementById("kyuw");
 let imgZ = document.getElementById("kyub");
 
 function ch1() {
-    document.getElementById("kyuw").src = "images/kyuremw.png"
+  document.getElementById("kyuw").src = "images/kyuremw.png"
 }
 function ch2() {
   document.getElementById("kyub").src = "images/kyuremb.png"
+}
+function revert() {
+  document.getElementById("kyuw").src = "images/reshi.png"
+  document.getElementById("kyub").src = "images/zek.png"
 }
 // document.getElementById("white").style.visibility = "visible";
