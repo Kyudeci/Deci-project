bca0c25f498f37555bde0607cba7d6bc07a13f80
Kyu <kyudeci@gmail.com>
2018-11-03 23:28:38 -0400

    More button adjustments

--- test.js
+++ test.js
@@ -1,17 +1,17 @@
-let buttonW = document.getElementById("white");
-let buttonB = document.getElementById("black");
-let buttonR = document.getElementById("return");
-let imgR = document.getElementById("kyuw");
-let imgZ = document.getElementById("kyub");
-
+//Need to create a rule that allows only one corrupt or disables second button.
 function ch1() {
   document.getElementById("kyuw").src = "images/kyuremw.png"
+  document.getElementById("kyuo").style.visibility = "hidden"
+  document.getElementById("return").style.visibility = "visible"
 }
 function ch2() {
   document.getElementById("kyub").src = "images/kyuremb.png"
+  document.getElementById("kyuo").style.visibility = "hidden"
+  document.getElementById("return").style.visibility = "visible"
 }
 function revert() {
   document.getElementById("kyuw").src = "images/reshi.png"
   document.getElementById("kyub").src = "images/zek.png"
+  document.getElementById("kyuo").style.visibility = "visible"
+  document.getElementById("return").style.visibility = "hidden"
 }
-// document.getElementById("white").style.visibility = "visible";
