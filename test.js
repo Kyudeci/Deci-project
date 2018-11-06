f857450cefc94bc8f3f925eb216a2b66a956db35
Shakiem Johnson <sjohnson@nyc.yearup.org>
2018-11-06 10:52:42 -0500

    created fuse functions that prompt

--- test.js
+++ test.js
@@ -4,17 +4,38 @@
 // let x = document.getElementById("return")
 // let y = document.getElementById("kyuw").src = "images/kyuremw.png"
 // let z = document.getElementById("kyub").src = "images/kyuremb.png"
+function fuse() {
+  if (confirm("You are about to fuse Kyurem and Reshiram! Are you sure about this?")) {
+    ch1()
+  }else{
+    return 1
+  }
+}
 function ch1() {
   document.getElementById("kyuw").src = "images/kyuremw.png"
   document.getElementById("kyuo").style.visibility = "hidden"
   document.getElementById("return").style.visibility = "visible"
   document.getElementById("black").style.visibility = "hidden"
 }
+function fuse2() {
+  if (confirm("You are about to fuse Kyurem and Zekrom! Are you sure about this?")) {
+    ch2()
+  }else{
+    return 1
+  }
+}
 function ch2() {
   document.getElementById("kyub").src = "images/kyuremb.png"
   document.getElementById("kyuo").style.visibility = "hidden"
   document.getElementById("return").style.visibility = "visible"
   document.getElementById("white").style.visibility = "hidden"
+}
+function defuse() {
+  if (confirm("Return Kyurem to normal?")) {
+    revert()
+  }else{
+    return 1
+  }
 }
 function revert() {
   document.getElementById("kyuw").src = "images/reshi.png"
